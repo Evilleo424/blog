6cd1251ef19e417dded239ad9ef5d8b93d66089c
Evilleo424 <linleosm@126.com>
2017-07-19 16:48:42 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,7 @@ Route::get('/', 'PostController@index');
 
 //文章列表页
 Route::get('/posts', 'PostController@index');
-//详情页
-Route::get("/posts/{post}", 'PostController@show');
+
 
     Route::get('excel/export', 'ExcelController@export');
     Route::get('excel/import', 'ExcelController@import');
@@ -26,7 +25,8 @@ Route::get("/posts/{post}", 'PostController@show');
     Route::group(['prefix' => 'posts','middleware' => 'before'], function () {
         //创建
         Route::get('/create', 'PostController@create');
-        
+        //详情页
+	Route::get("/{post}", 'PostController@show');
         //保存
         Route::post('/', 'PostController@store');
         //编辑
